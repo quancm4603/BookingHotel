c8be500ae71e2b0d05cde1c79d0babf4281dd7a7
Quan Cao Minh <quancm4603@gmail.com>
2024-10-31 23:36:53 +0700

    Add logout and logic redirect login

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,26 +1,65 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'expo-router';
+import { auth } from '@/firebaseConfig'; 
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { Button, View } from 'react-native';
 
 export default function TabLayout() {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log('User is signed in');
+        setLoading(false);
+      } else {
+        console.log('User is signed out');
+        router.replace('/login');
+      }
+    });
+
+    return () => unsubscribe();
+  }, [router]);
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      router.replace('/login'); // Chuyển hướng đến trang đăng nhập sau khi đăng xuất
+    } catch (error) {
+      console.error("Error signing out: ", error);
+    }
+  };
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <h1>Loading...</h1>
+      </View>
+    );
+  }
 
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-        }}
-      />
-    </Tabs>
+    <View style={{ flex: 1 }}>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Home',
+          }}
+        />
+        <Tabs.Screen
+          name="explore"
+          options={{
+            title: 'Explore',
+          }}
+        />
+      </Tabs>
+      <Button title="Logout" onPress={handleLogout} />
+    </View>
   );
 }
